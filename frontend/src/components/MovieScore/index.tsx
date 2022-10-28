e7be940d3db15d2ca7e039a29172fd9d7f32f068
Fulano de Tal <felipesand824@gmail.com>
2022-10-28 13:16:36 -0300

    Show Score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,17 +1,21 @@
 import MovieStars from "components/MovieStars";
 import './style.css';
 
-function MovieScore() {
+type Props = {
+  count: number;
+  score: number;
+}
 
-    const score = 3.5;
-    const count = 13;
+
+function MovieScore({count, score}: Props) {
+
 
   return (
     <div className="dsmovie-score-container">
       <p className="dsmovie-score-value">
         {score > 0 ? score.toFixed(1) : "-"}
       </p>
-      <MovieStars></MovieStars>
+      <MovieStars score={score}></MovieStars>
       <p className="dsmovie-score-count">{count} avaliações</p>
     </div>
   );
--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,17 +1,53 @@
 import { ReactComponent as StarFull } from "assets/img/star-full.svg";
 import { ReactComponent as StarHalf } from "assets/img/star-half.svg";
 import { ReactComponent as StarEmpty } from "assets/img/star-empty.svg";
+import "./style.css";
 
-import './style.css';
+type Props = {
+  score: number;
+};
 
-function MovieStars() {
+type StarProps = {
+  fill: number;
+};
+
+function getFills(score: number) {
+  const fills = [0, 0, 0, 0, 0];
+
+  const integerPart = Math.floor(score);
+
+  for (let i = 0; i < integerPart; i++) {
+    fills[i] = 1;
+  }
+
+  const diff = score - integerPart;
+  if (diff > 0) {
+    fills[integerPart] = 0.5;
+  }
+
+  return fills;
+}
+
+function Star({ fill }: StarProps) {
+  if (fill === 0) {
+    return <StarEmpty></StarEmpty>;
+  } else if (fill === 1) {
+    return <StarFull></StarFull>;
+  } else {
+    return <StarHalf></StarHalf>;
+  }
+}
+
+function MovieStars({ score }: Props) {
+  const fills = getFills(score);
+
   return (
     <div className="dsmovie-stars-container">
-      <StarFull></StarFull>
-      <StarFull></StarFull>
-      <StarFull></StarFull>
-      <StarHalf></StarHalf>
-      <StarEmpty></StarEmpty>
+      <Star fill={fills[0]}></Star>
+      <Star fill={fills[1]}></Star>
+      <Star fill={fills[2]}></Star>
+      <Star fill={fills[3]}></Star>
+      <Star fill={fills[4]}></Star>
     </div>
   );
 }
